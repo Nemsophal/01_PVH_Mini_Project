3fa8d762b26cfe042d66371b59da0d368bc5e089
Nem Sophal <nemsophal7@gmail.com>
2026-03-03 15:44:07 +0700

    modified save controller

--- src/main/java/controllers/SaveController.java
+++ src/main/java/controllers/SaveController.java
@@ -25,31 +25,14 @@ public class SaveController {
     }
 
     public void saveUpdate() {
-        List buf = dao.getUpdateBuffer();
-        if (buf.isEmpty()) {
-            ProductsView.showErrorMessage("No updates to save!");
-            return;
-        }
-        ProductsView.showUpdateBuffer(buf);
-        dao.saveUpdateBuffer();
-        ProductsView.showSuccessMessage("All updates saved to database!");
+
     }
 
     public void unsaveInsert() { // VIEW ONLY
-        List buf = dao.getInsertBuffer();
-        if (buf.isEmpty()) {
-            ProductsView.showErrorMessage("No pending inserts.");
-            return;
-        }
-        ProductsView.showInsertBuffer(buf); // just show
+
     }
 
     public void unsaveUpdate() { // VIEW ONLY
-        List buf = dao.getUpdateBuffer();
-        if (buf.isEmpty()) {
-            ProductsView.showErrorMessage("No pending updates.");
-            return;
-        }
-        ProductsView.showUpdateBuffer(buf); // just show, nothing else
+
     }
 }
