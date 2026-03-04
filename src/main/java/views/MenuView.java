d8f6a90e7ed4a113c066dbb5fa23c9f444739d3c
Nem Sophal <nemsophal7@gmail.com>
2026-03-04 13:17:19 +0700

    update validateion

--- src/main/java/views/MenuView.java
+++ src/main/java/views/MenuView.java
@@ -5,7 +5,7 @@ public class MenuView {
         System.out.println("   N. Next Page      P. Previous Page    F. First Page    L. Last Page    G. Goto");
         System.out.println();
         System.out.println("W) Write          R) Read (id)       U) Update        D) Delete        S) Search (name)    Se) Set rows");
-        System.out.println("sa) Save          Un) Unsaved        Ba) Backup       Re) Restore      E) Exit");
+        System.out.println("Sa) Save          Un) Unsaved        Ba) Backup       Re) Restore      E) Exit");
         System.out.println("---------------------------------------------------------------------------------------------------------");
     }
     public static void showGoodbye() {
